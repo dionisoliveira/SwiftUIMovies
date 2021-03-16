baa3cd0080b7251e693c612335430f4efd3a1a24
Dionis Silva Oliveira <dionis.oliveira@montreal.com.br>
2021-03-16 17:41:02 -0300

    .

---
+++ MoviesSwiftUI/MoviesSwiftUI/IoC/RegisterIoC.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterIoC.swift
+//  MoviesSwiftUI
+//
+//  Created by Montreal on 12/03/21.
+//
+
+import Foundation
+
+
+struct RegisterIoC{
+    init() {
+        RegisterItens()
+    }
+    
+    
+     func RegisterItens(){
+        //Set class for register in IOC
+        DependencyInjector.register(dependency: HttpHelper() as HttpHelperProtocol)
+       // DependencyInjector.register(dependency: CurrencyViewModel() as CurrencyViewModelProtocol)
+        //DependencyInjector.register(dependency: LoginAdm() as LoginAdmProtocol)
+      
+    }
+}
